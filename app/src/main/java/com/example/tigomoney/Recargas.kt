08f3bcd82c21a6b47aadac823de666617a869f04
robertocastillo <robertocastillo945@gmail.com>
2021-06-02 12:11:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tigomoney/Recargas.kt
@@ -0,0 +1,11 @@
+package com.example.tigomoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recargas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recargas)
+    }
+}
